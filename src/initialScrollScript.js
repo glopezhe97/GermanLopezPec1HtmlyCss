547e869b8d0a30125a4688d8a621d87c08ccfa00
glopezhe97 <gerlohe97@gmail.com>
2025-01-11 10:46:06 +0100

    final asyncscript

---
+++ src/initialScrollScript.js
@@ -0,0 +1,6 @@
+// Inicialización de ScrollReveal
+ScrollReveal({
+    duration: 2000, 
+    reset: false,
+    once: true
+});
--- src/scriptScrollReveal.js
+++ src/scriptScrollReveal.js
@@ -1,25 +1,12 @@
-window.onload = function () {
-    // Añadir la clase 'loaded' cuando toda la página haya cargado completamente
-    document.body.classList.add('loaded');
-
-    // Inicialización de ScrollReveal
-    ScrollReveal({
-        duration: 2000, 
-        reset: false,
-        once: true
-    });
-
-    // Configuración de ScrollReveal para revelar los elementos
+document.addEventListener('DOMContentLoaded', () => {
     ScrollReveal().reveal('.headerscroll', {
-        beforeReveal: (el) => el.classList.remove('load-hidden')
+        beforeReveal: (el) => el.classList.remove('load-hidden'),
     });
-
     ScrollReveal().reveal('.h1_notlema', {
-        beforeReveal: (el) => el.classList.remove('load-hidden')
+        beforeReveal: (el) => el.classList.remove('load-hidden'),
     });
-
     ScrollReveal().reveal('.articlescroll', {
         interval: 750,
-        beforeReveal: (el) => el.classList.remove('load-hidden')
+        beforeReveal: (el) => el.classList.remove('load-hidden'),
     });
-};
+});
